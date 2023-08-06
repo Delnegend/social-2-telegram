502f8c7f98322cb47a263c574f42e59be32d6fa3
Delnegend <kiennguyen19323@gmail.com>
2023-08-06 17:55:33 +0700

    feat(overwrite_sm_name): :sparkles: ko-fi, buymeacoffee

--- helpers/overwrite_sm_name.py
+++ helpers/overwrite_sm_name.py
@@ -4,5 +4,9 @@ def overwrite_sm_name(old_name: str) -> str:
             return old_name.replace("Twitter", "𝕏")
         case foo if "FA" in foo:
             return old_name.replace("FA", "FA 🐾")
+        case foo if "Ko-fi" in foo:
+            return old_name.replace("Ko-fi", "Ko-fi 🍵")
+        case foo if "buymeacoffee" in foo:
+            return old_name.replace("buymeacoffee", "buymeacoffee 🍵")
         case _:
             return old_name
