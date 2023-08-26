ee50bb881bd487ec31a667747ba0447d84c31bdb
Delnegend <kiennguyen19323@gmail.com>
2023-08-26 14:27:01 +0700

    feat(NewArtist): :sparkles: restructure artist alt handles variable, parse alt handles from sm links

--- classes/NewArtist.py
+++ classes/NewArtist.py
@@ -3,7 +3,8 @@ from dataclasses import dataclass, field
 
 from option import Option, Some
 
-from variables import Colors
+from helpers.insensitive_match import insensitive_match  # type: ignore
+from variables.Message import NewArtistMsg
 
 
 @dataclass
@@ -18,19 +19,21 @@ class NewArtist:
         self,
         twitter_username: str,
         artists_info: dict[str, ArtistInfoData],
-        artists_alt_account: dict[str, str],
+        artists_alt_handles: dict[str, set[str]],
     ) -> None:
-        self.__twitter_username = twitter_username
+        self.__artist_handle = twitter_username
         self.__artists_info = artists_info
-        self.__artists_alt_accounts = artists_alt_account
+        self.__artists_alt_handles = artists_alt_handles
 
-    def __process_representing_hashtags(self, _input: str) -> str:
-        hashtags = " ".join([hashtag.replace("#", "").strip() for hashtag in _input.split(" ") if hashtag.strip()])
-        hashtags = hashtags or self.__twitter_username
+    def __parse_represent_hashtags(self, in_hashtag_str: str) -> str:
+        hashtags = set(
+            [hashtag.replace("#", "").strip() for hashtag in in_hashtag_str.split(" ")] + [self.__artist_handle]
+        )
+        hashtags.discard("")
+        hashtags = " ".join(hashtags)
         return hashtags
 
-    def __process_social_media_links(self, _input: str) -> dict[str, str]:
-        # unprocessed_links = _input.split(",")
+    def __parse_social_media_links(self, _input: str) -> dict[str, str]:
         unprocessed_links = [
             link
             for link in _input.split(",")
@@ -42,46 +45,76 @@ class NewArtist:
             processed_links[link[0].strip()] = ":".join(link[1:]).strip()
         return processed_links
 
-    def __update_alt_accounts_list(self, social_media: dict[str, str]) -> None:
-        # if there are 2 or more social_media_name.lower() starting with "twitter" then
-        # add
-        # - account_alt_1: account_main
-        # - account_alt_2: account_main
-        # - ...
-        # to self.__artists_alt_accounts
-        artist_twitter_links = [
-            link
-            for social_media_title, link in social_media.items()
-            if social_media_title.lower().startswith("twitter")
-        ]
-        if len(artist_twitter_links) < 2:
-            return
-        original_twitter_username = re.sub(r"https://twitter.com/|/$", "", artist_twitter_links[0])
-        for link in artist_twitter_links[1:]:
-            link = re.sub(r"https://twitter.com/|/$", "", link)
-            self.__artists_alt_accounts[link] = original_twitter_username
+    def process_alt_handles(self, social_media: dict[str, str]) -> set[str]:
+        alt_handles: set[str] = set()
+
+        def helper(pattern: str, val: str, match_group: int = 1):
+            nonlocal alt_handles
+            if match := re.search(pattern, val):
+                alt_handles.add(match.group(match_group))
+
+        for _, link in social_media.items():
+            match link:
+                case link if "twitter.com" in link.lower():
+                    helper(r"twitter.com\/([^/]+)\/?", link)
+                case link if "instagram.com" in link.lower():
+                    helper(r"instagram.com\/([^/]+)\/?", link)
+                case link if "furaffinity.net" in link.lower():
+                    helper(r"furaffinity.net\/user\/([^/]+)\/?", link)
+                case link if "patreon.com" in link.lower():
+                    helper(r"patreon.com\/([^/]+)\/?", link)
+                case link if "gumroad.com" in link.lower():
+                    helper(r"(?:https?:\/\/)?(?:www\.)?([^\/]+)\.gumroad\.com\/?", link)
+                case link if "skeb.jp" in link.lower():
+                    helper(r"skeb\.jp\/@([^/]+)\/?", link)
+                case link if "ko-fi.com" in link.lower():
+                    helper(r"ko-fi\.com\/([^/]+)\/?", link)
+                case link if "linktr.ee" in link.lower():
+                    helper(r"linktr\.ee\/([^/]+)\/?", link)
+                case link if "t.me" in link.lower():
+                    helper(r"t\.me\/([^/]+)\/?", link)
+                case link if "fanbox.cc" in link.lower():
+                    helper(r"(?:https?:\/\/)?(?:www\.)?([^\/]+)\.fanbox\.cc\/?", link)
+                case link if "itaku.ee" in link.lower():
+                    helper(r"itaku\.ee\/profile\/([^/]+)\/?", link)
+                case link if "picarto.tv" in link.lower():
+                    helper(r"picarto\.tv\/([^/]+)\/?", link)
+                case _:
+                    pass
+        if match := insensitive_match(self.__artist_handle, self.__artists_alt_handles.keys()):
+            alt_handles.update(self.__artists_alt_handles[match.value])
+        alt_handles.discard("")
+        alt_handles.discard(self.__artist_handle)
+        return self.__rm_dupl_handles(alt_handles)
+
+    def __rm_dupl_handles(self, alt_handles: set[str]) -> set[str]:
+        """Set is case sensitive, this will make it case insensitive"""
+        lowered: set[str] = set()
+        result: set[str] = set()
+        for handle in alt_handles:
+            if handle.lower() not in lowered:
+                lowered.add(handle.lower())
+                result.add(handle)
+        return result
 
     def new(self) -> Option[str]:
         """Return 0 if user wants to exit"""
-        hashtag_representing_artist = input(
-            f"Enter hashtag(s) representing artist ({Colors.YELLOW}{self.__twitter_username}{Colors.END}): "
-        ).strip()
-        if hashtag_representing_artist.startswith("0"):
+        handle = self.__artist_handle
+
+        if (hashtag_represent := input(NewArtistMsg.HASHTAG_REPRESENT.format(handle)).strip()).startswith("0"):
             return Some("0")
-        hashtag_representing_artist = self.__process_representing_hashtags(hashtag_representing_artist)
+        hashtag_represent = self.__parse_represent_hashtags(hashtag_represent)
 
-        social_media = input("Enter artist's social media links (format: <name>: <link>, ...): ").strip()
-        if social_media.startswith("0"):
+        if (social_media := input(NewArtistMsg.SOCIAL_MEDIA.format(handle)).strip()).startswith("0"):
             return Some("0")
-        social_media = self.__process_social_media_links(social_media)
+        social_media = self.__parse_social_media_links(social_media)
 
-        self.__update_alt_accounts_list(social_media)
+        self.__artists_alt_handles[handle] = self.process_alt_handles(social_media)
 
-        country_flag = input("Enter artist's country flag (format: emoji): ").strip()
-        if country_flag.startswith("0"):
+        if (country_flag := input(NewArtistMsg.COUNTRY.format(handle)).strip()).startswith("0"):
             return Some("0")
 
-        self.__artists_info[self.__twitter_username] = ArtistInfoData(
-            country_flag=country_flag, hashtag_represent=hashtag_representing_artist, social_media=social_media
+        self.__artists_info[self.__artist_handle] = ArtistInfoData(
+            country_flag=country_flag, hashtag_represent=hashtag_represent, social_media=social_media
         )
         return Some("")
