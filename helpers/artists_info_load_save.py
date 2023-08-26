efe814b2a6e969f91fcc8b2ece03c98eb771a873
Delnegend <kiennguyen19323@gmail.com>
2023-08-26 14:32:35 +0700

    feat(artists_info_load_save.py): :sparkles: restructure alt handles variable

--- helpers/artists_info_load_save.py
+++ helpers/artists_info_load_save.py
@@ -3,15 +3,17 @@ import os
 import yaml
 
 from classes.NewArtist import ArtistInfoData
+from variables.Config import Config
 
 
-def artists_info_load() -> tuple[dict[str, ArtistInfoData], dict[str, str]]:
+def artists_info_load() -> tuple[dict[str, ArtistInfoData], dict[str, set[str]]]:
     """Load artist info from yaml files and return them as a tuple of dicts."""
-    if not os.path.isfile("artist_info.yaml"):
-        with open(file="artist_info.yaml", mode="w", encoding="utf-8") as f:
+
+    if not os.path.isfile(Config.ARTISTS_INFO_FILE):
+        with open(file=Config.ARTISTS_INFO_FILE, mode="w", encoding="utf-8") as f:
             yaml.dump({}, f, sort_keys=False, allow_unicode=True, indent=4)
 
-    with open(file="artist_info.yaml", mode="r", encoding="utf-8") as f:
+    with open(file=Config.ARTISTS_INFO_FILE, mode="r", encoding="utf-8") as f:
         data = yaml.load(f, Loader=yaml.FullLoader).items()
         artists_info = {
             artist_username: ArtistInfoData(
@@ -22,22 +24,21 @@ def artists_info_load() -> tuple[dict[str, ArtistInfoData], dict[str, str]]:
             for artist_username, artist_info in data
         }
 
-    if not os.path.isfile("artist_alt_account.yaml"):
-        with open(file="artist_alt_account.yaml", mode="w", encoding="utf-8") as f:
+    if not os.path.isfile(Config.ARTISTS_ALT_HANDLES_FILE):
+        with open(file=Config.ARTISTS_ALT_HANDLES_FILE, mode="w", encoding="utf-8") as f:
             yaml.dump({}, f, sort_keys=False, allow_unicode=True, indent=4)
 
-    with open(file="artist_alt_account.yaml", mode="r", encoding="utf-8") as f:
-        data = yaml.load(f, Loader=yaml.FullLoader).items()
-        artists_alt_handles = {alt_account: main_account for alt_account, main_account in data}
+    with open(file=Config.ARTISTS_ALT_HANDLES_FILE, mode="r", encoding="utf-8") as f:
+        artists_alt_handles = yaml.load(f, Loader=yaml.FullLoader).items()
 
-    return artists_info, artists_alt_handles
+    return dict(artists_info), dict(artists_alt_handles)
 
 
-def artists_info_save(artists_info: dict[str, ArtistInfoData], artists_alt_handles: dict[str, str]) -> None:
-    with open(file="artist_info.yaml", mode="w", encoding="utf-8") as f:
+def artists_info_save(artists_info: dict[str, ArtistInfoData], artists_alt_handles: dict[str, set[str]]) -> None:
+    with open(file=Config.ARTISTS_INFO_FILE, mode="w", encoding="utf-8") as f:
 
         def process_sm_links(sm: dict[str, str]) -> dict[str, str]:
-            res = {}
+            res: dict[str, str] = {}
             for sm_name, sm_link in sm.items():
                 sm_link = sm_link.strip().replace("http://", "https://")
                 if not sm_link.endswith("/"):
@@ -56,6 +57,12 @@ def artists_info_save(artists_info: dict[str, ArtistInfoData], artists_alt_handl
         artists_info_yaml = dict(sorted(artists_info_yaml.items(), key=lambda item: item[0]))
         yaml.dump(artists_info_yaml, f, sort_keys=False, allow_unicode=True, indent=4)
 
-    with open(file="artist_alt_account.yaml", mode="w", encoding="utf-8") as f:
+    with open(file=Config.ARTISTS_ALT_HANDLES_FILE, mode="w", encoding="utf-8") as f:
         artists_alt_handles = dict(sorted(artists_alt_handles.items(), key=lambda item: item[0]))
-        yaml.dump(artists_alt_handles, f, sort_keys=False, allow_unicode=True, indent=4)
+        yaml.dump(
+            {handle: list(alt_handles) for handle, alt_handles in artists_alt_handles.items()},
+            f,
+            sort_keys=False,
+            allow_unicode=True,
+            indent=4,
+        )
