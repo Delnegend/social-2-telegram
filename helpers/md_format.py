876b6c87adaba5358c30581f14e57e4eddff0ed6
Delnegend <kiennguyen19323@gmail.com>
2023-09-07 11:39:39 +0700

    fix(PlatformTwitter): :bug: square brackets got mixed with MD links

--- helpers/md_format.py
+++ helpers/md_format.py
@@ -7,6 +7,7 @@ def md_format(content: str) -> str:
     # replace [...](...) with @...@@...@
     for match in re.findall(r"\[(.*?)\]\((.*?)\)", content):
         content = content.replace(f"[{match[0]}]({match[1]})", f"@{match[0]}@@{match[1]}@")
+    content = content.replace("SquareBracStart", "[").replace("SquareBracEnd", "]")
 
     # Add prefix "\" to special characters
     for char in "_*[]()~`>#-+={}.!|":
